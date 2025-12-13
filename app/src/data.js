1005c5a32d777789f3a470057b46b7c3e4a8051e
pranali <kokatepranu833@gmail.com>
2025-12-13 14:40:52 +0530

    Create data.js
    basic changes added

---
+++ app/src/data.js
@@ -0,0 +1,23 @@
+const questions = [
+  {
+    question: "What is the capital of India?",
+    options: ["Mumbai", "Delhi", "Kolkata", "Chennai"],
+    answer: "Delhi",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    answer: "Mars",
+  },
+  {
+    question: "Who invented the telephone?",
+    options: [
+      "Albert Einstein",
+      "Alexander Graham Bell",
+      "Isaac Newton",
+      "Nikola Tesla",
+    ],
+    answer: "Alexander Graham Bell",
+  },
+];
+export default questions;
