75836d9eebdedf844ddb1bd89d96111e359a271f
pranali <kokatepranu833@gmail.com>
2025-12-21 18:14:03 +0530

    Update App.js
    changes added

--- form/src/App.js
+++ form/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import RegistrationForm from "./assets/RegistractionForm.jsx";
+import RegistrationForm from "./RegistractionForm.jsx";
 import "./App.css";
 export default function App (){
   return(
